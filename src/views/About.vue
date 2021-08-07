d84680454a289b58dc1328974d7859cb3bec6963
samwise <974049754@qq.com>
2021-08-08 01:42:42 +0800

    add biying wallpaper

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img :src="biyingPic" alt="" />
   </div>
 </template>
+<script>
+import { ref } from "@vue/reactivity";
+import axios from "axios";
+export default {
+  setup() {
+    let biyingPic = ref("");
+    axios
+      .get("/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
+      .then((res) => {
+        console.log("=====", res.data.images[0].url);
+        biyingPic.value = `https://cn.bing.com${res.data.images[0].url}`;
+      })
+      .catch((err) => {
+        console.log("-----", err);
+      });
+    return {
+      biyingPic,
+    };
+  },
+};
+</script>
+
