433cd997a0cee2e667c14917edfc2e7b314c2b75
samwise <974049754@qq.com>
2021-08-08 16:21:35 +0800

    add devserver proxy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
     devServer: {
-        proxy: 'https://cn.bing.com'
+        proxy: {
+            '/HPImageArchive.aspx': {
+                target: 'https://cn.bing.com',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    },
+    pwa: {
+        iconPaths: {
+            favicon32: 'favicon.ico',
+            favicon16: 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon: 'favicon.ico',
+            msTileImage: 'favicon.ico'
+        }
     }
 }
