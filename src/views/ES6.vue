b8d2c3631033a53f47be531cd29fd55e2064d313
samwise-coder <houguochao@outlook.com>
2021-09-06 23:18:30 +0800

    iterator

--- src/views/ES6.vue
+++ src/views/ES6.vue
@@ -11,18 +11,22 @@
 <script>
 export default {
   setup() {
-    var target = function () {
-      return "I am the target";
-    };
-    var handler = {
-      apply: function () {
-        return "I am the proxy";
-      },
-    };
+    var it = makeIterator(["a", "b"]);
 
-    var p = new Proxy(target, handler);
+    it.next(); // { value: "a", done: false }
+    it.next(); // { value: "b", done: false }
+    it.next(); // { value: undefined, done: true }
 
-    console.log(p());
+    function makeIterator(array) {
+      var nextIndex = 0;
+      return {
+        next: function () {
+          return nextIndex < array.length
+            ? { value: array[nextIndex++], done: false }
+            : { value: undefined, done: true };
+        },
+      };
+    }
 
     return {};
   },
