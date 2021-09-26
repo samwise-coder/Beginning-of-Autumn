40f7dee8893b9bd4d3599ec782d055aa41365061
samwise-coder <houguochao@outlook.com>
2021-09-26 23:38:44 +0800

    add interface

--- src/views/Ts.vue
+++ src/views/Ts.vue
@@ -33,6 +33,28 @@ export default {
         }, 100 * i);
       }
     }
+    //9.26接口
+    interface SquareConfig {
+      color?: string;
+      width?: number;
+    }
+
+    function createSquare(config: SquareConfig): {
+      color: string;
+      area: number;
+    } {
+      let newSquare = { color: "white", area: 100 };
+      if (config.color) {
+        newSquare.color = config.color;
+      }
+      if (config.width) {
+        newSquare.area = config.width * config.width;
+      }
+      return newSquare;
+    }
+
+    let mySquare = createSquare({ color: "black" });
+    console.log(mySquare);
     return {
       dataType,
       variableDeclare,
