fc6772bf4b1916362b136c20d5532298ec3f5554
samwise-coder <houguochao@outlook.com>
2021-10-28 22:20:14 +0800

    vue3 delete native

--- src/views/Reg.vue
+++ src/views/Reg.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <h1>reg</h1>
+    <self-vmodel v-on:focus="handle"></self-vmodel>
   </div>
 </template>
 
 <script>
+import SelfVmodel from "../components/SelfVmodel";
 export default {
+  components: {
+    SelfVmodel,
+  },
+  data() {
+    return {
+      lovingVue: "true",
+    };
+  },
+  methods: {
+    handle() {
+      console.log("999");
+    },
+  },
   mounted() {
     var str = "Vis9it Runoob!9 r|r9Unhahaha!";
     var n = str.replace(/^vis\d/i, "sss");
