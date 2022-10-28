9f5c703fe16de8d6d28ed5fb67d047a456b3ea7d
samwise-coder <houguochao@outlook.com>
2022-10-29 00:09:19 +0800

    feat:canvas增加时钟动画

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -3,26 +3,124 @@
     <canvas id="canvas"></canvas>
     <canvas id="canvas2"></canvas>
     <canvas id="canvas3" width="400" height="400"></canvas>
+    <canvas id="canvas4" width="400" height="400"></canvas>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
+    clock() {
+      var now = new Date();
+      var ctx = document.getElementById("canvas4").getContext("2d");
+      ctx.save();
+      ctx.clearRect(0, 0, 150, 150);
+      ctx.translate(75, 75);
+      ctx.scale(0.4, 0.4);
+      ctx.rotate(-Math.PI / 2);
+      ctx.strokeStyle = "black";
+      ctx.fillStyle = "white";
+      ctx.lineWidth = 8;
+      ctx.lineCap = "round";
+
+      // Hour marks
+      ctx.save();
+      for (var i = 0; i < 12; i++) {
+        ctx.beginPath();
+        ctx.rotate(Math.PI / 6);
+        ctx.moveTo(100, 0);
+        ctx.lineTo(120, 0);
+        ctx.stroke();
+      }
+      ctx.restore();
+
+      // Minute marks
+      ctx.save();
+      ctx.lineWidth = 5;
+      for (i = 0; i < 60; i++) {
+        if (i % 5 != 0) {
+          ctx.beginPath();
+          ctx.moveTo(117, 0);
+          ctx.lineTo(120, 0);
+          ctx.stroke();
+        }
+        ctx.rotate(Math.PI / 30);
+      }
+      ctx.restore();
+
+      var sec = now.getSeconds();
+      var min = now.getMinutes();
+      var hr = now.getHours();
+      hr = hr >= 12 ? hr - 12 : hr;
+
+      ctx.fillStyle = "black";
+
+      // write Hours
+      ctx.save();
+      ctx.rotate(
+        hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+      );
+      ctx.lineWidth = 14;
+      ctx.beginPath();
+      ctx.moveTo(-20, 0);
+      ctx.lineTo(80, 0);
+      ctx.stroke();
+      ctx.restore();
+
+      // write Minutes
+      ctx.save();
+      ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+      ctx.lineWidth = 10;
+      ctx.beginPath();
+      ctx.moveTo(-28, 0);
+      ctx.lineTo(112, 0);
+      ctx.stroke();
+      ctx.restore();
+
+      // Write seconds
+      ctx.save();
+      ctx.rotate((sec * Math.PI) / 30);
+      ctx.strokeStyle = "#D40000";
+      ctx.fillStyle = "#D40000";
+      ctx.lineWidth = 6;
+      ctx.beginPath();
+      ctx.moveTo(-30, 0);
+      ctx.lineTo(83, 0);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(95, 0, 10, 0, Math.PI * 2, true);
+      ctx.stroke();
+      ctx.fillStyle = "rgba(0,0,0,0)";
+      ctx.arc(0, 0, 3, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.restore();
+
+      ctx.beginPath();
+      ctx.lineWidth = 14;
+      ctx.strokeStyle = "#325FA2";
+      ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+      ctx.stroke();
+
+      ctx.restore();
+
+      window.requestAnimationFrame(this.clock);
+    },
     faa() {
       var ctx = document.getElementById("canvas3").getContext("2d");
       let canvasDom = document.getElementById("canvas3");
       var img = new Image();
       img.onload = function () {
         ctx.drawImage(img, 100, 100, 50, 50);
-        let dataUrl = canvasDom.toDataURL("image/jpeg", 0.5);
+        let dataUrl = canvasDom.toDataURL("image/jpeg", 0.7);
         console.log(dataUrl);
       };
       img.src = require("@/assets/logo.png");
       // img.src =
       //   "data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==";
     },
-
     // 参考资料：https://zhuanlan.zhihu.com/p/28257724
     foo() {
       document.addEventListener("touchmove", function (e) {
@@ -88,6 +186,7 @@ export default {
   },
   mounted() {
     // 参考资料：https://github.com/sunshine940326/canvas-nest
+    window.requestAnimationFrame(this.clock);
     this.foo();
     this.faa();
     class Circle {
