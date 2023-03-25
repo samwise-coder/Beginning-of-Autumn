0372f0db24508b3e35ca3da99f297a6e5905ec57
samwise-coder <houguochao@outlook.com>
2023-03-25 21:39:53 +0800

    feat:日常学习打卡

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="hello">
-    <em>第一</em><em>第二</em><em>第三</em>
-    <p>第四</p>
-    <p>第五</p>
-    <p>第六</p>
+    <h1>Simple grid example</h1>
+
+    <div class="container">
+      <div>One</div>
+      <div>Two</div>
+      <div>Three</div>
+      <div>Four</div>
+      <div>Five</div>
+      <div>Six</div>
+      <div>Seven</div>
+    </div>
   </div>
 </template>
 
@@ -18,4 +25,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container > div {
+  border-radius: 5px;
+  padding: 10px;
+  background-color: rgb(207, 232, 220);
+  border: 2px solid rgb(79, 185, 227);
+}
+.container {
+  display: grid;
+  grid-template-columns: 200px 200px 200px;
+}
 </style>
